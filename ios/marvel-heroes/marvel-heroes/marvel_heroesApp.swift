0434e3f613c7d7b979c8e66542d9804bdc6dd2f8
Хвостенко Вячеслав <40929439+AsTeFu@users.noreply.github.com>
2021-12-22 14:22:45 +0600

    feat: added ios app

---
+++ ios/marvel-heroes/marvel-heroes/marvel_heroesApp.swift
@@ -0,0 +1,17 @@
+//
+//  marvel_heroesApp.swift
+//  marvel-heroes
+//
+//  Created by drezvanova on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct marvel_heroesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
